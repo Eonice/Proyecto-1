e9d799c93e16cb565ecee24402adeb9d8fe4a868
David Miralles <davidmiralles22@gmail.com>
2023-09-20 21:32:05 +0200

    Agregar archivos de proyecto.

---
+++ Proyecto 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_1
+{
+    public class Ejemplo1_4_2
+    {
+        public static void Main()
+        {
+            Console.WriteLine(12345 + 67890);
+        }
+    }
+}
