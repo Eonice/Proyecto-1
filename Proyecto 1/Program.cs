85b1215c8d687d37447f5734ee72f9aa13f471e2
David Miralles <davidmiralles22@gmail.com>
2023-09-21 19:48:31 +0200

    ReadLine multiplicacion

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -8,11 +8,17 @@ using System.Threading.Tasks;
 
 namespace Proyecto_1
 {
-    public class Ejemplo1_4_2
+    public class Ejemplo_multiplicacion
     {
         public static void Main()
         {
-            Console.WriteLine(12345 + 67890);
+            int primernumero, segundonumero, multiplicacion;
+            Console.WriteLine("Introduce el primer numero");
+            primernumero = System.Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero");
+            segundonumero = System.Convert.ToInt32(Console.ReadLine());
+            multiplicacion = primernumero * segundonumero;
+            Console.WriteLine("La multiplicacion entre {0} y {1} es {2}", primernumero, segundonumero, multiplicacion);
         }
     }
 }
