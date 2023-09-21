47a08a07b3a09edd711dcb5cc52ad6836ccd2c97
David Miralles <davidmiralles22@gmail.com>
2023-09-21 20:43:29 +0200

    Operaciones y ejercicios

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proyecto_1
 {
-    public class millas_to_metros
+    public class Write
     {
         public static void Main()
         {
-            int millas, metros;
-            Console.WriteLine("Introduce las millas");
-            millas = System.Convert.ToInt32(Console.ReadLine());
-            metros = millas * 1852;
-            Console.WriteLine("{0} millas equivalen a {1} metros", millas, metros);
+            int a, b;
+            System.Console.Write("Introduce el primer dato ");
+            a = System.Convert.ToInt32(System.Console.ReadLine());
+            System.Console.Write("Introduce el segundo dato ");
+            b = System.Convert.ToInt32(Console.ReadLine());
+            int multiplicacion = (a + b) * (a - b);
+            int resta = (2 * a - 2 * b);
+            System.Console.WriteLine("multiplicacion y la resta de ambos es {2} y la resta del doble de cada uno es {3}", a, b, multiplicacion, resta);
         }
     }
 }
+    
